f8f08f31410fc24da62263ac482ec58867397420
randymay <randy.may@blaazinsoftware.com>
2016-01-07 22:26:48 -0500

    Initial commit

---
+++ src/main/java/com/blaazinsoftware/module1/ClassUnderTest.java
@@ -0,0 +1,12 @@
+package com.blaazinsoftware.module1;
+
+/**
+ * @author Randy May
+ *         Date: 2016-01-07
+ */
+public class ClassUnderTest {
+
+    public int addTwoNumbers(int x, int y) {
+        return x + y;
+    }
+}
---
+++ src/test/java/com/blaazinsoftware/module1/ClassUnderTestTest.java
@@ -0,0 +1,11 @@
+package com.blaazinsoftware.module1;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Randy May
+ *         Date: 2016-01-07
+ */
+public class ClassUnderTestTest {
+
+}
