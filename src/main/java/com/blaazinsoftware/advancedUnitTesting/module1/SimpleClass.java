0eaa8fa01a4a89adf43c3599dee7698a7976f49b
randymay <randy.may@blaazinsoftware.com>
2016-01-07 23:09:57 -0500

    Refactor to include artifact in package name.

--- src/main/java/com/blaazinsoftware/advancedUnitTesting/module1/SimpleClass.java
+++ src/main/java/com/blaazinsoftware/advancedUnitTesting/module1/SimpleClass.java
@@ -1,11 +1,10 @@
-package com.blaazinsoftware.module1;
+package com.blaazinsoftware.advancedUnitTesting.module1;
 
 /**
  * @author Randy May
  *         Date: 2016-01-07
  */
 public class SimpleClass {
-
     public int addTwoNumbers(int x, int y) {
         return x + y;
     }
